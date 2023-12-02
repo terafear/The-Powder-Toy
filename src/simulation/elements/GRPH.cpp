e85558c1fdf29f4578f6bb4fc786bcf2e015a76f
terafear <32689400+terafear@users.noreply.github.com>
2023-12-01 20:38:02 -0600

    Added graphene element

---
+++ src/simulation/elements/GRPH.cpp
@@ -0,0 +1,104 @@
+#include "simulation/ElementCommon.h"
+
+static int update(UPDATE_FUNC_ARGS);
+
+void Element::Element_GRPH()
+{
+    Identifier = "DEFAULT_PT_GRPH";
+    Name = "GRPH";
+    Colour = 0x303030_rgb;
+    MenuVisible = 1;
+    MenuSection = SC_SOLIDS;
+    Enabled = 1;
+
+    Advection = 0.0f;
+    AirDrag = 0.01f * CFDS;
+    AirLoss = 0.95f;
+    Loss = 0.00f;
+    Collision = 0.0f;
+    Gravity = 0.0f;
+    Diffusion = 0.00f;
+    HotAir = 0.000f * CFDS;
+    Falldown = 1;
+
+    Flammable = 0;
+    Explosive = 0;
+    Meltable = 1;
+    Hardness = 50;
+
+    Weight = 100;
+
+    HeatConduct = 251; // High thermal conductivity
+    Description = "Graphene. Extremely conductive, strong, and light.";
+
+    Properties = TYPE_SOLID | PROP_CONDUCTS | PROP_HOT_GLOW | PROP_LIFE_DEC;
+
+    LowPressure = IPL;
+    LowPressureTransition = NT;
+    HighPressure = IPH;
+    HighPressureTransition = NT;
+    LowTemperature = ITL;
+    LowTemperatureTransition = NT;
+    HighTemperature = 3652.0f; // Melting point of Graphene
+    HighTemperatureTransition = PT_LAVA;
+
+    Update = &update;
+}
+
+static int update(UPDATE_FUNC_ARGS)
+{
+    float temp = parts[i].temp;
+
+    // Temperature equalization with neighboring particles
+    for (int rx = -1; rx <= 1; rx++)
+    {
+        for (int ry = -1; ry <= 1; ry++)
+        {
+            if ((rx || ry) && (x + rx >= 0) && (y + ry >= 0) && (x + rx < XRES) && (y + ry < YRES))
+            {
+                int r = sim->pmap[y + ry][x + rx];
+                if (!r)
+                    continue;
+
+                int rt = TYP(r);
+                float rtemp = parts[ID(r)].temp;
+                float temp_diff = temp - rtemp;
+
+                // Thermal conductivity
+                if (abs(temp_diff) > 5.0f)
+                {
+                    parts[i].temp -= temp_diff * 0.5f;
+                    parts[ID(r)].temp += temp_diff * 0.5f;
+                }
+            }
+        }
+    }
+
+    // Electrical conductivity logic
+    // Example: Decrease conductivity at high temperature
+    if (temp > 2000.0f)
+    {
+        // Reduced conductivity
+        parts[i].ctype = PT_NONE;
+    }
+    else
+    {
+        // Restored conductivity
+        parts[i].ctype = PT_SPRK;
+    }
+
+    // Glow effect based on temperature
+    if (temp > 2500.0f)
+    {
+        // Apply a glow effect
+        parts[i].life = 4; // Using life to indicate glowing state
+    }
+
+    // Melting behavior
+    if (temp >= 3652.0f)
+    {
+        sim->part_change_type(i, x, y, PT_LAVA);
+    }
+
+    return 0;
+}
